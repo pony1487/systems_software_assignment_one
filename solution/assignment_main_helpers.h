3fc7dcadd730f39e784d5b76343bce0ed7fa4fa3
pony1487 <connollyro@gmail.com>
2019-03-21 17:59:56 +0000

    deleted all master code so merge is easier

--- solution/assignment_main_helpers.h
+++
@@ -1,6 +0,0 @@
-#ifndef ASSIGNMENT_MAIN_H_   /* Include guard */
-#define ASSIGNMENT_MAIN_H_
-
-void runTimeCheck();
-
-#endif
--- solution/time_check.h
+++
@@ -1,8 +0,0 @@
-#ifndef TIME_CHECK_H_   /* Include guard */
-#define TIME_CHECK_H_
-#include <time.h>
-
-int check_backup_time(struct tm *);
-struct tm* get_current_time();
-
-#endif
